a6c06870c0127b5bd02fd615196dfcc8caa363aa
arakanenagasebros-beep <arakane.nagasebros@gmail.com>
2026-03-17 09:15:26 +0900

    Update config.js

--- config.js
+++ config.js
@@ -1,3 +1,3 @@
 window.APP_CONFIG = {
-  DEFAULT_API_URL: ""
+  DEFAULT_API_URL: "https://script.google.com/macros/s/AKfycbwfRe4sk0lCeyrbYZSg4ubZnM8CC7cj89RM-bKFQ0d7R855qazmbRJuEK_SDjraFgMd/exec"
 };
